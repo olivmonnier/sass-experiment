e566add450d6e7ba099c5e75f614096c7316cc5a
Olivier Monnier <monnier@indigo-net.com>
2019-09-06 14:50:37 +0200

    WIP

---
+++ main.js
@@ -0,0 +1,23 @@
+function checkCssUsed() {
+  const listCssUsed = getCssUsed();
+
+  const clsUnused = listCssUsed
+    .map(layer => Object.values(layer)[0])
+    .flat()
+    .filter(c => document.querySelectorAll(`.${c}`).length === 0);
+
+  if (clsUnused.length > 0) {
+    console.warn('Unused classes list: ');
+    console.table(clsUnused);
+  }
+}
+
+function getCssUsed() {
+  const clsUsed = getComputedStyle(document.documentElement)
+    .getPropertyValue('--cls-used')
+    .replace(/(^\(|\)$)/g, '');
+
+  return clsUsed.split(', ')
+    .map(layer => layer.split(': '))
+    .map(([layer, deps]) => ({ [layer]: deps.replace(/"/g, '').split(' ') }));
+}
