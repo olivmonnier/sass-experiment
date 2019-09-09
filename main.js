06ac113b8ae65ae6ec477cb659f2acd78827f31a
Olivier Monnier <monnier@indigo-net.com>
2019-09-09 11:49:44 +0200

    vscode settings

--- main.js
+++ main.js
@@ -4,26 +4,34 @@ function checkCssUnused() {
   return listCssUsed
     .map(({ layer, dependencies }) => ({
       layer,
-      dependencies: dependencies
-        .filter(dep => document.querySelectorAll(`.${dep}`).length === 0)
+      dependencies: dependencies.filter(
+        dep => document.querySelectorAll(`.${dep}`).length === 0
+      )
     }))
-    .filter(({dependencies}) => dependencies.length > 0);
+    .filter(({ dependencies }) => dependencies.length > 0);
 }
 
 function getCssUsed() {
   const clsUsed = getComputedStyle(document.documentElement)
-    .getPropertyValue('--cls-used')
-    .replace(/(^ \(|\)$)/g, '');
+    .getPropertyValue("--cls-used")
+    .replace(/(^ \(|\)$)/g, "");
 
-  return clsUsed.split(', ')
-    .map(layer => layer.split(': '))
-    .map(([layer, deps]) => ({ layer, dependencies: deps.replace(/"/g, '').split(' ')}))
+  return clsUsed
+    .split(", ")
+    .map(layer => layer.split(": "))
+    .map(([layer, deps]) => ({
+      layer,
+      dependencies: deps.replace(/"/g, "").split(" ")
+    }));
 }
 
 function showClassesUnused(cls) {
-  console.warn('Unused classes list: ');
-  console.table(cls.map(({ layer, dependencies }) => 
-    ({ layer, dependencies: dependencies.join(', ') })),
+  console.warn("Unused classes list: ");
+  console.table(
+    cls.map(({ layer, dependencies }) => ({
+      layer,
+      dependencies: dependencies.join(", ")
+    })),
     ["layer", "dependencies"]
   );
 }
